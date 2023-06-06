6b76476fb8efb757ded530395307db64c098318c
José da Silva <jose.d.silva@tecnico.ulisboa.pt>
2023-06-06 22:05:44 +0100

    finish SnailShellPattern

--- src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
@@ -1,5 +1,11 @@
 package com.premiumminds.internship.snail;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 /**
@@ -14,6 +20,54 @@ class SnailShellPattern implements ISnailShellPattern {
    * @return order array of values thar represent a snail shell pattern
    */
   public Future<int[]> getSnailShell(int[][] matrix) {
-    throw new RuntimeException("Not Implemented Yet");
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
+    Future<int[]> future = executorService.submit(() -> {
+      List<Integer> result = new ArrayList<Integer>();
+
+      int line, col, begin, end, size;
+      line = 0;
+      begin = 0;
+      col = 0;
+      end = matrix.length - 1;
+      size = matrix.length;
+
+      while (begin < size && end >= begin && begin != end){
+        while (col >= begin && line == begin && col <= end){
+          result.add(matrix[line][col]);
+          line++;
+        }
+
+        col--;
+
+        while (col >= begin && line == end){
+          result.add(matrix[line][col]);
+          col--;
+        }
+
+        line--;
+
+        while (line > begin && col == begin){
+          result.add(matrix[line][col]);
+          line--;
+        }
+
+        begin++;
+        end--;
+      }
+
+
+
+
+      int[] resultArray = new int[size];
+        for (int i = 0; i < size; i++) {
+            resultArray[i] = result.get(i);
+        }
+
+        return resultArray;
+
+    });
+
+    executorService.shutdown();
+    return future;
   };
 }
