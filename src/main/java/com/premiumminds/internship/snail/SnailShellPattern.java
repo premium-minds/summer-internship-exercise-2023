d24df3179a127e2ff4752530ea9ae3a62466a268
José da Silva <jose.d.silva@tecnico.ulisboa.pt>
2023-06-08 21:34:20 +0100

    final version

--- src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
@@ -10,64 +10,114 @@ import java.util.concurrent.Future;
 
 /**
  * Created by aamado on 05-05-2023.
+ *
+ * Last edited by josedsilva20 on 06-08-2023
  */
 class SnailShellPattern implements ISnailShellPattern {
+
+  // Declared here to use them in all methods
+  // (instead of a big method with all variables inside)
+  private int line, col, begin = 0;
+  private int end, size;
+  private int[][] matrix;
+  private List<Integer> result = new ArrayList<>();
+
 
   /**
    * Method to get snailshell pattern
    * 
-   * @param matrix matrix of numbers to go through
+   * @param startMatrix matrix of numbers to go through
    * @return order array of values thar represent a snail shell pattern
    */
-  public Future<int[]> getSnailShell(int[][] matrix) {
+  public Future<int[]> getSnailShell(int[][] startMatrix) {
     ExecutorService executorService = Executors.newSingleThreadExecutor();
     Future<int[]> future = executorService.submit(() -> {
-      List<Integer> result = new ArrayList<Integer>();
 
-      int line, col, begin, end, size;
-      line = 0;
-      begin = 0;
-      col = 0;
+      // Link private matrix to the original one
+      matrix = startMatrix;
+
+      // checks if is an empty matrix
+      if (matrix == null || matrix.length == 0) {
+        return new int[0];
+      }
+
+      //checks if is a non-square matrix
+      for (int[] ints : matrix)
+        if (ints.length < matrix.length)
+          return new int[0];
+
       end = matrix.length - 1;
       size = matrix.length;
 
+      // Makes a full turn
       while (begin < size && end >= begin && begin != end){
-        while (col >= begin && line == begin && col <= end){
-          result.add(matrix[line][col]);
-          line++;
-        }
+        addFirstLine();
 
-        col--;
+        line++; // Jump to next line
+        col--;  // Reestablish column number to n - 1
 
-        while (col >= begin && line == end){
-          result.add(matrix[line][col]);
-          col--;
-        }
+        addLastColumn();
 
-        line--;
+        col--;  // Advance to middle columns
+        line--; // Reestablish line number to n - 1
 
-        while (line > begin && col == begin){
-          result.add(matrix[line][col]);
-          line--;
-        }
+        addLastLine();
 
+        col++;  // Go to first column
+        line--; // Go up in snail
+
+        addFirstColumn();
+
+        // Change to (n-1)x(n-1) Matrix
         begin++;
         end--;
+        line++;
+        col++;
       }
 
+      // Add the element in the middle of matrix
+      if (begin == end)
+        result.add(matrix[begin][end]);
 
+      // Prepare and copy the returnable array
+      int[] resultArray = new int[result.size()];
+      for (int i = 0; i < result.size(); i++) {
+        resultArray[i] = result.get(i);
+      }
 
-
-      int[] resultArray = new int[size];
-        for (int i = 0; i < size; i++) {
-            resultArray[i] = result.get(i);
-        }
-
-        return resultArray;
+      return resultArray;
 
     });
 
     executorService.shutdown();
     return future;
   };
+
+  public void addFirstLine(){
+    while (col >= begin && line == begin && col <= end){
+      result.add(matrix[line][col]);
+      col++;
+    }
+  }
+
+  public void addLastColumn(){
+    while (col == end && line <= end){
+      result.add(matrix[line][col]);
+      line++;
+    }
+  }
+
+  public void addLastLine(){
+    while (col >= begin && line == end){
+      result.add(matrix[line][col]);
+      col--;
+    }
+  }
+
+  public void addFirstColumn(){
+    while (line > begin && col == begin){
+      result.add(matrix[line][col]);
+      line--;
+    }
+  }
 }
