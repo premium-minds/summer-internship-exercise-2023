abef9c3035bfe391eb968ae627d732b350071b47
powy-e <edfnazario@gmail.com>
2023-06-11 17:12:45 +0100

    Submission

--- src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
@@ -1,19 +1,143 @@
 package com.premiumminds.internship.snail;
 
-import java.util.concurrent.Future;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
 
 /**
  * Created by aamado on 05-05-2023.
  */
 class SnailShellPattern implements ISnailShellPattern {
 
-  /**
-   * Method to get snailshell pattern
-   * 
-   * @param matrix matrix of numbers to go through
-   * @return order array of values thar represent a snail shell pattern
-   */
-  public Future<int[]> getSnailShell(int[][] matrix) {
-    throw new RuntimeException("Not Implemented Yet");
-  };
+    /**
+     * Method to get snailshell pattern
+     *
+     * @param matrix matrix of numbers to go through
+     * @return order array of values thar represent a snail shell pattern
+     */
+    public Future<int[]> getSnailShell(final int[][] matrix) {
+        // Initialize new executor with 1 thread to submit all other callables
+        final ExecutorService initExecutor = Executors.newSingleThreadExecutor();
+
+        // Initialize new executor with 4 threads or the number of processors available
+        final ExecutorService executor = Executors.newFixedThreadPool(Math.min(Runtime.getRuntime().availableProcessors(), 4));
+
+        // Submit a new callable that submits all other callables
+
+        return initExecutor.submit(new Callable<int[]>() {
+            @Override
+            public int[] call() throws Exception {
+
+                // get matrix length
+                final int len = matrix.length;
+                // initialize sorted array with size N*N
+                final int[] sorted = new int[len * len];
+
+                // Half of the length of the matrix
+                final int halfLen = (int) Math.floor((len + 1) / 2.0);
+
+                // List to keep track of all the callables
+                List<Callable<int[]>> matrixParts = new ArrayList<>();
+
+                // Horizontal right rows
+                matrixParts.add(new Callable<int[]>() {
+                    @Override
+                    public int[] call() {
+                        if (len == 0) return sorted;
+
+                        int copyLen = len;
+                        int destPos = 0;
+                        // for each row
+                        for (int i = 0; i < halfLen; i++) {  // for every row until half of the matrix
+
+                            // copy horizontal right
+                            System.arraycopy(matrix[i], i, sorted, destPos, copyLen);
+                            destPos += copyLen * 4 - 4;
+                            copyLen -= 2;
+                        }
+                        return sorted;
+                    }
+                });
+
+                // Vertical down columns
+                matrixParts.add(new Callable<int[]>() {
+                    @Override
+                    public int[] call() {
+                        if (len == 0) return sorted;
+
+                        int copyLen = len;
+                        int destPos = len;
+                        // for each column
+                        for (int i = 0; i < halfLen; i++) {
+                            copyLen -= 2;
+                            // copy vertical down
+                            for (int j = i + 1; j <= copyLen + i; j++) {
+                                sorted[destPos] = matrix[j][len - 1 - i];
+                                destPos += 1;
+                            }
+                            destPos += copyLen * 3 + 2; // One small copy len already accounted for
+                        }
+
+                        return sorted;
+                    }
+                });
+
+                // Horizontal left rows
+                matrixParts.add(new Callable<int[]>() {
+                    @Override
+                    public int[] call() {
+                        if (len == 0) return sorted;
+
+                        int copyLen = len;
+                        int destPos = len * 2 - 2;
+                        // for each row
+                        for (int i = 0; i < halfLen; i++) {
+                            // copy horizontal left
+                            for (int j = i; j < copyLen + i; j++) {
+                                sorted[destPos] = matrix[len - 1 - i][len - 1 - j];
+                                destPos += 1;
+                            }
+                            destPos += copyLen * 3 - 8;
+                            copyLen -= 2;
+                        }
+
+
+                        return sorted;
+                    }
+                });
+
+                // Vertical up colums
+                matrixParts.add(new Callable<int[]>() {
+                                    @Override
+                                    public int[] call() {
+                                        if (len == 0) return sorted;
+
+                                        int copyLen = len;
+                                        int destPos = len * 3 - 2;
+                                        // for each column
+                                        for (int i = 0; i < halfLen; i++) {
+                                            copyLen -= 2;
+                                            // copy vertical up
+                                            for (int j = i + 1; j <= copyLen + i; j++) {
+                                                sorted[destPos] = matrix[len - 1 - j][i];
+                                                destPos += 1;
+                                            }
+                                            destPos += copyLen * 3 - 2;
+                                        }
+
+                                        return sorted;
+                                    }
+
+
+                                }
+                );
+
+                executor.invokeAll(matrixParts);  // Wait for all callables to finish
+                return sorted;
+            }
+        });
+
+    }
+
 }
--- src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
+++ src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
@@ -1,6 +1,7 @@
 package com.premiumminds.internship.snail;
 
 import static org.junit.Assert.assertArrayEquals;
+
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
@@ -16,22 +17,55 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class SnailShellPatternTest {
 
-  /**
-   * The corresponding implementations to test.
-   *
-   * If you want, you can make others :)
-   *
-   */
-  public SnailShellPatternTest() {
-  };
+    /**
+     * The corresponding implementations to test.
+     * <p>
+     * If you want, you can make others :)
+     */
+    public SnailShellPatternTest() {
+    }
 
-  @Test
-  public void ScreenLockinPatternTestFirst3Length2Test()
-      throws InterruptedException, ExecutionException, TimeoutException {
-    int[][] matrix = { { 1, 2, 3 }, { 8, 9, 4 }, { 7, 6, 5 } };
-    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
-    int[] result = count.get(10, TimeUnit.SECONDS);
-    int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    assertArrayEquals(result, expected);
-  }
+    @Test
+    public void ScreenLockinPatternTestFirst3Length2Test()
+            throws InterruptedException, ExecutionException, TimeoutException {
+        int[][] matrix = {{1, 2, 3}, {8, 9, 4}, {7, 6, 5}};
+        Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+        int[] result = count.get(10, TimeUnit.SECONDS);
+        int[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        assertArrayEquals(result, expected);
+    }
+
+    // Test 4x4 matrix
+    @Test
+    public void ScreenLockinPatternTestFirst4Length2Test()
+            throws InterruptedException, ExecutionException, TimeoutException {
+        int[][] matrix = {{1, 2, 3, 4}, {12, 13, 14, 5}, {11, 16, 15, 6}, {10, 9, 8, 7}};
+        Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+        int[] result = count.get(10, TimeUnit.SECONDS);
+        int[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+        assertArrayEquals(result, expected);
+    }
+
+    // Test for empty matrix
+    @Test
+    public void ScreenLockinPatternTestFirst0Length2Test()
+            throws InterruptedException, ExecutionException, TimeoutException {
+        int[][] matrix = {};
+        Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+        int[] result = count.get(10, TimeUnit.SECONDS);
+        int[] expected = {};
+        assertArrayEquals(result, expected);
+    }
+
+    // Test speed but not correctness of the algorithm
+    @Test
+    public void BigTest()
+            throws InterruptedException, ExecutionException {
+        int n = 22_000;
+        int[][] matrix = new int[n][n];
+
+        Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+        count.get();
+    }
+
 }
